899c488684f275f9f4ddf9ff97b27c5c6a30c2ec
greynomeng <46880037+greynomeng@users.noreply.github.com>
2026-04-28 11:38:26 +1000

    Fixed image on home page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row">
-        <img src="recipeBook.jpg" className="max-w-xl rounded-lg shadow-2xl" />
+        <Image
+          src="/recipeBook.jpg"
+          alt=""
+          width={600}
+          height={500}
+          className="max-w-xl rounded-lg shadow-2xl"
+        />
         <div>
           <h1 className="text-5xl font-bold">Recipes!</h1>
           <p className="py-6">A collection of simple, tasty recipes.</p>
