4bb6298fc7dccf0ca8feb6bfa38ce77eb73d4dd8
greynomeng <46880037+greynomeng@users.noreply.github.com>
2026-04-29 09:43:46 +1000

    Included loading spinner and empty state in recipes page

--- app/(pages)/recipes/page.jsx
+++ app/(pages)/recipes/page.jsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 import { LuPlus } from "react-icons/lu";
 import { useFetch } from "@/app/hooks/useFetch";
 import RecipeCard from "@/app/components/RecipeCard";
+import LoadingSpinner from "@/app/components/LoadingSpinner";
+import EmptyState from "@/app/components/EmptyState";
 
 export default function RecipesPage() {
   const { data: recipes, loading, refetch } = useFetch("/api/recipes");
@@ -28,14 +30,29 @@ export default function RecipesPage() {
       />
 
       {/* Recipe cards */}
-      <div className="flex justify-center w-full">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 w-full">
-          {recipes &&
-            recipes.map((recipe) => (
-              <RecipeCard key={recipe._id.toString()} recipe={recipe} />
-            ))}
+      {loading ? (
+        <LoadingSpinner />
+      ) : recipes?.length === 0 ? (
+        <EmptyState
+          title="No recipes yet"
+          description="Add recipes by clicking the Add Recipe button..."
+          action={
+            <button className="btn btn-primary gap-1" onClick={openAdd}>
+              <LuPlus />
+              Add Recipe
+            </button>
+          }
+        />
+      ) : (
+        <div className="flex justify-center w-full">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 w-full">
+            {recipes &&
+              recipes.map((recipe) => (
+                <RecipeCard key={recipe._id.toString()} recipe={recipe} />
+              ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
