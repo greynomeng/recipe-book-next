9869a16dd1958215531ab22e314a5b993b97135d
greynomeng <46880037+greynomeng@users.noreply.github.com>
2026-04-29 06:31:10 +1000

    Recipes page PageHeader

--- app/(pages)/recipes/page.jsx
+++ app/(pages)/recipes/page.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+// @ts-nocheck
+"use client";
+
+import PageHeader from "@/app/components/PageHeader";
+import { useState } from "react";
+import { LuPlus } from "react-icons/lu";
+import { useFetch } from "@/app/hooks/useFetch";
 
 export default function RecipesPage() {
-  return <div>RecipesPage</div>;
+  const { data: recipes, loading, refetch } = useFetch("/api/recipes");
+  const [selected, setSelected] = useState(null);
+  const [search, setSearch] = useState("");
+
+  const openAdd = () => {};
+
+  return (
+    <>
+      {/* Header */}
+      <PageHeader
+        title="Recipes"
+        subtitle={`${recipes?.length || 0} payees`}
+        action={
+          <button className="btn btn-primary gap-1" onClick={openAdd}>
+            <LuPlus />
+            Add Payee
+          </button>
+        }
+      />
+    </>
+  );
 }
